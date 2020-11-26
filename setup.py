908778354e6824b6be28f2271fdb8b659b3b1dbb
Heiko Schmidle <heiko.schmidle@gmail.com>
2020-11-26 11:09:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
-import os
 from setuptools import setup
 
-
 setup(
-    name = "demo",
-    version = "0.0.1",
-    author = "Heiko Schmidle",
-    description = ("An demonstration fro Nextjournal"),
-    license = "BSD",
-    keywords = "Nextjournal"
+    name="demo",
+    version="1.0",
+    author="Heiko Schmidle",
+    description="Demo for Nextjournal",
+    py_modules = ['demo']
 )
