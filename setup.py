4b6288d034bc2b9e7b951eba98a820ee8f7aa08e
Heiko Schmidle <heiko.schmidle@gmail.com>
2020-11-26 11:47:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
-from setuptools import setup
+import setuptools
+from distutils.core import setup
+
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
-    name="demo",
-    version="1.0",
-    author="Heiko Schmidle",
-    description="Demo for Nextjournal",
-    py_modules = ['demo']
+    name='demo',
+    version='1.0',
+    #packages=['src/agda_kernel'],
+    packages=setuptools.find_packages(where="src"),
+    package_dir={""},
+    description='Demo',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='Heiko Schmidle',
+    url='https://github.com/heikoschmidle/demo',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ]
 )
